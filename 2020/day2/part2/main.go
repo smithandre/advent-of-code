a0aa515a2c2e99a710ceab3d0944153e082882ff
Andre Smith <andresm@miway.co.za>
2020-12-30 22:24:13 +0200

    Added skeletons for day 2 and 3

---
+++ 2020/day2/part2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	input := readFile("day2/input.txt")
+	fmt.Println(input)
+}
+
+func readFile(filename string) []string {
+	file, err := os.Open(os.Getenv("GOPATH") + "/src/adventofcode/2020/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
