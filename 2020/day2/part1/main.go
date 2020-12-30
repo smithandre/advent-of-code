7e58135c21cd20ba3d047f6ddacfa14fd5b29de1
Andre Smith <andresm@miway.co.za>
2020-12-30 23:46:21 +0200

    Day 2 Part 1 and 2 solutions

--- 2020/day2/part1/main.go
+++ 2020/day2/part1/main.go
@@ -5,11 +5,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	input := readFile("day2/input.txt")
-	fmt.Println(input)
+	var valids int
+	for _,val := range input {
+		parts := strings.Fields(val)
+
+		searchChar := parts[1][:1]
+
+		s := strings.Split(parts[0], "-")
+
+		min,_ := strconv.Atoi(s[0])
+		max,_ := strconv.Atoi(s[1])
+		count := strings.Count(parts[2], searchChar)
+		if count < min || count > max {
+			fmt.Println("Char "+searchChar+" not found between", min, "and", max, "times in "+parts[2])
+		} else {
+			fmt.Println("Char "+searchChar+" found between", min, "and", max, "times in "+parts[2]+"("+strconv.Itoa(count)+")")
+			valids++
+		}
+	}
+	fmt.Println("Valid Passwords", valids)
 }
 
 func readFile(filename string) []string {
--- 2020/day2/part2/main.go
+++ 2020/day2/part2/main.go
@@ -5,11 +5,32 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	input := readFile("day2/input.txt")
-	fmt.Println(input)
+	var valids int
+	for _,val := range input {
+		parts := strings.Fields(val)
+
+		searchChar := parts[1][:1]
+
+		s := strings.Split(parts[0], "-")
+
+		pos1,_ := strconv.Atoi(s[0])
+		pos2,_ := strconv.Atoi(s[1])
+
+		str := strings.Split(parts[2], "")
+		if (str[pos1-1] == searchChar && str[pos2-1] != searchChar) || (str[pos1-1] != searchChar && str[pos2-1] == searchChar) {
+			fmt.Println("Char "+searchChar+" found at position", pos1, "or", pos2, " in "+parts[2])
+			valids++
+		} else {
+			fmt.Println("Char "+searchChar+" not found at position", pos1, "or", pos2, " in "+parts[2])
+		}
+	}
+	fmt.Println("Valid Passwords", valids)
 }
 
 func readFile(filename string) []string {
